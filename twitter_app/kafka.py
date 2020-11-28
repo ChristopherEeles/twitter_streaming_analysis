d6d6a8d8e2c73476126ddee85e1385b4e81f70fc
ChristopherEeles <christopher.eeles@outlook.com>
2020-11-28 16:39:40 -0500

    Update: Producer and consumer now working; added zookeeper to client configuration

--- twitter_app/kafka.py
+++ twitter_app/kafka.py
@@ -1,4 +1,4 @@
 from pykafka import KafkaClient
 
 # Connect to the kafka server running on Amazon EC2 virtual machine
-client = KafkaClient(hosts='18.221.88.167:9092')
+client = KafkaClient(hosts='18.221.88.167:9092', zookeeper_hosts='18.221.88.167:2181')
