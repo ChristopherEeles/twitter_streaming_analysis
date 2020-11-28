83701b321544edac1f36222d992c9d3b3d2b781a
ChristopherEeles <christopher.eeles@outlook.com>
2020-11-28 12:39:24 -0500

    Update: Added AWS IP for Kafka server

--- twitter_app/kafka.py
+++ twitter_app/kafka.py
@@ -1,6 +1,4 @@
 from pykafka import KafkaClient
 
 # Connect to the kafka server running on Amazon EC2 virtual machine
-client = KafkaClient(hosts='3.139.77.133:9092')
-
-
+client = KafkaClient(hosts='18.221.88.167:9092')
